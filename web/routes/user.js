45d3ca51675d2d2176a86aa2383bc5b33489860f
DinushaNaveen <dldndasanayaka@gmail.com>
2018-05-23 08:55:19 +0530

    remove duplication of user accounts

--- web/routes/user.js
+++ web/routes/user.js
@@ -6,33 +6,44 @@ const bcrypt = require('bcrypt');
 const User = require('../../model/user');
 
 router.post('/signup', (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err){
-            return res.status(500).json({
-                error: err
-            });
-        } else {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                name: req.body.name,
-                nicName: req.body.nicName,
-                password: hash
-            });
-            user
-                .save()
-                .then(result => {
-                    res.status(201).json({
-                        message: 'User Created',
-                    });
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: 'mail Exists'
                 })
-                .catch(err => {
-                    res.status(500).json({
-                        error: err
-                    });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            name: req.body.name,
+                            nicName: req.body.nicName,
+                            password: hash
+                        });
+                        user
+                            .save()
+                            .then(result => {
+                                res.status(201).json({
+                                    message: 'User Created',
+                                });
+                            })
+                            .catch(err => {
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
                 });
-        }
-    });
+            }
+        })
+
 });
 
 module.exports = router;
