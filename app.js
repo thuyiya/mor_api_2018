4d733169de5781014930ecd2c89a797ca34994eb
DinushaNaveen <dldndasanayaka@gmail.com>
2018-05-23 08:44:16 +0530

    user creating process completed

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-import http from 'http';
-import { app } from './web';
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const mongoose = require('mongoose');
+const userRoutes = require('./web/routes/user');
+const bodyParser = require('body-parser');
+const app = express();
+const config = require('./config/database');
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,3 +16,17 @@ http.createServer(app).listen(PORT, err => {
     console.log(`App listen to port: ${PORT}`);
   }
 });
+
+const connectDB = mongoose.connect(config.database);
+if(connectDB){
+  console.log("Database connected")
+}else{
+  console.log("Warning! Database not connected")
+}
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/user', userRoutes);
